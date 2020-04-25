adf5611ad047c26a104c3e08b24fead04914caea
RC Maples <rc@rcmaples.io>
2020-04-25 16:47:47 -0400

    Populating cards and styling them.

--- js/scratch.js
+++ js/scratch.js
@@ -1,6 +1,5 @@
-'use strict';
-
 // Dump the Division Keys to array for mapping
+'use strict';
 
 const data = {
   kind: 'civicinfo#representativeInfoResponse',
@@ -309,60 +308,68 @@ const data = {
   ],
 };
 
-const divKeys = Object.keys(data.divisions);
-
 const coalesceInfo = (
   { name, party, photoUrl, address, channels, urls },
   officeName = ''
 ) => {
-  let twitter;
+  console.log(name, address);
+  let twitter, facebook;
   if (!!channels) {
-    twitter = channels.filter((channel) => {
+    channels.filter((channel) => {
       if (channel.type === 'Twitter') {
-        return channel.id;
+        twitter = channel.id;
+      }
+      if (channel.type === 'Facebook') {
+        facebook = channel.id;
       }
     });
   }
 
-  $('.container').append(`
+  $('.container').append(
+    `
+    <div class="card">
     <h4>${officeName}</h4>
-    <p>${name} - ${party}</p>
-    <img src="${photoUrl}" width="200px" alt="${name}"/>
-    <p>
-
-    <a href="${urls[0]}">Website</a>
-    </p>
-
-  `);
-
-  if (!!address) {
-    address.map((address) => {
-      console.log('Address:');
-      for (const property in address) {
-        console.log(`\t${property}: ${address[property]}`);
-      }
-    });
-  }
-
-  if (!!channels) {
-    console.log('Social Media:');
-    channels.map((channel) => {
-      console.log('\t', channel.type, channel.id);
-    });
-  }
+    <p>${name} <br /> ${party}</p>
+    ` +
+      (!!photoUrl
+        ? `<img src="${photoUrl}" width="200px" alt="${name}"/>`
+        : `<img src="../assets/no_photo.jpg" width="200px" alt="${name}"/>`) +
+      '<p>' +
+      (!!twitter
+        ? `<a href="https://twitter.com/${twitter}" target="_blank" rel="noopener noreferrer">Twitter</a>`
+        : '') +
+      ' ' +
+      (!!facebook
+        ? `<a href="https://facebook.com/${facebook}" target="_blank" rel="noopener noreferrer">Facebook</a>`
+        : '') +
+      ' ' +
+      (!!urls[0]
+        ? `<a href="${urls[0]}" target="_blank" rel="noopener noreferrer">Website</a>`
+        : '') +
+      '</p>' +
+      (!!address
+        ? 'Address: <br/>' +
+          (!!address[0].line1 ? `${address[0].line1} <br />` : '') +
+          (!!address[0].line2 ? `${address[0].line2} <br />` : '') +
+          `${address[0].city}, ${address[0].state} ${address[0].zip}`
+        : 'No address available.') +
+      '</div>'
+  );
 };
 
+const divKeys = Object.keys(data.divisions);
+
 // Map over the divKeys array to get Division Name
-divKeys.map((key) => {
-  const offices = data.offices.filter((office) => office.divisionId === key);
-  offices.map((office) => {
-    const officeName = office.name;
-    if (office.officialIndices.length === 1) {
-      coalesceInfo(data.officials[office.officialIndices], officeName);
-    } else {
-      office.officialIndices.map((index) => {
-        coalesceInfo(data.officials[index], officeName);
-      });
-    }
-  });
-});
+// divKeys.map((key) => {
+//   const offices = data.offices.filter((office) => office.divisionId === key);
+//   offices.map((office) => {
+//     const officeName = office.name;
+//     if (office.officialIndices.length === 1) {
+//       coalesceInfo(data.officials[office.officialIndices], officeName);
+//     } else {
+//       office.officialIndices.map((index) => {
+//         coalesceInfo(data.officials[index], officeName);
+//       });
+//     }
+//   });
+// });
